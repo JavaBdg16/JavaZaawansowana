1290951a9f17729228e68614fe117304bda4d1b1
Marcin Szupke <marcin.szupke.sda@gmail.com>
2021-10-03 12:06:06 +0200

    exceptions

---
+++ src/main/java/exceptions/Main.java
@@ -0,0 +1,27 @@
+package exceptions;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int i = 12;
+        int j = 5;
+
+        try {
+
+            int result = i / (j - 2);
+            System.out.println(result);
+
+        } catch (Exception ex) {
+
+            System.out.println(ex.getMessage());
+
+        } finally {
+
+            System.out.println("finally");
+
+        }
+
+
+        System.out.println("Program zakończył się");
+    }
+}
---
+++ src/main/java/exceptions/MainFile.java
@@ -0,0 +1,41 @@
+package exceptions;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class MainFile {
+
+    public static void main(String[] args) {
+
+        BufferedReader reader = null;
+        try {
+
+            reader = new BufferedReader(new FileReader("file_name"));
+            String inputLine = null;
+            while ((inputLine = reader.readLine()) != null) {
+                System.out.println(inputLine);
+                String[] parts = inputLine.split(" ");
+                String operation = parts[0];
+                double leftValue = Double.parseDouble(parts[1]);
+                double rightValue = Double.parseDouble(parts[2]);
+            }
+
+        } catch (Exception ex) {
+
+            System.out.println("Error: " + ex.getMessage());
+
+        } finally {
+
+            try {
+                System.out.println("Closing file - file_name");
+                reader.close();
+            } catch (Exception e) {
+                System.out.println("Error closing file");
+            }
+
+        }
+
+    }
+
+}
