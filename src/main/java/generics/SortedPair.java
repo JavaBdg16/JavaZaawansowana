ffb46ff8b8aa894067cbb705107a3b831136e719
Marcin Szupke <marcin.szupke.sda@gmail.com>
2021-10-09 14:36:51 +0200

    sorted pair

--- src/main/java/generics/SortedPair.java
+++ src/main/java/generics/SortedPair.java
@@ -1,6 +1,8 @@
 package generics;
 
-public class SortedPair<T extends Comparable> {
+// Integer extends Comparable
+
+public class SortedPair<T extends Comparable<T>> {
 
     private final T first;
     private final T second;
